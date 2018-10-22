411ca581afb89680c825f037d345695d31fcf463
Thom9521 <Thom9521@edu.easj.dk>
2018-10-22 11:16:42 +0200

    Commit goooooooooooooo

---
+++ connection.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('sequelizedb', 'root', 'xmeto1234', {
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
+  const User = sequelize.define('user', {
+    firstName: {
+      type: Sequelize.STRING
+    },
+    lastName: {
+      type: Sequelize.STRING
+    }
+  });
+/*
+  // force: true will drop the table if it already exists
+  User.sync({force: true}).then(() => {
+    // Table created
+    return User.create({
+      firstName: 'Thomas',
+      lastName: 'Christensen'
+    });
+  });
+  */
+ User.findAll().then(users => {
+    console.log(users)
+  })
+  
